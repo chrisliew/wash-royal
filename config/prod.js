e72e84a53e201c7ce0b52a75545e04e6f8e269b0
chrisliew <chriswvliew@gmail.com>
2018-11-14 10:53:39 +0700

    fixed deploy bug in prod

--- config/prod.js
+++ config/prod.js
@@ -1,12 +1,12 @@
 // prod.js -- production keys here
 
 module.exports = {
-  mongoURI: 'process.env.MONGO_URI',
+  mongoURI: process.env.MONGO_URI,
   google: {
-    clientID: 'process.env.GOOGLE_CLIENT_ID',
-    clientSecret: 'process.env.CLIENT_SECRET',
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
   },
-  cookieKey: 'process.env.COOKIE_KEY',
+  cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY
 }
