cb565a4111eedd0d1826ec8fc2c9f0515ea819fb
chrisliew <chriswvliew@gmail.com>
2018-11-19 11:19:49 +0700

    Completed login and heroku deploy

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -11,7 +11,6 @@ class Login extends Component {
         Login Using:
         <br />
         <Button><a href="/auth/google">Sign In With Google+</a></Button>
-        <Button><a href="/auth/facebook">Sign in With Facebook</a></Button>
       </div>
     )
   }
