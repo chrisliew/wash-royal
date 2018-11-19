3ac6b0a4a995c710221ca559a3d724c8242a5e9f
chrisliew <chriswvliew@gmail.com>
2018-11-19 08:22:43 +0700

    Removed button to push to heroku

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -10,8 +10,8 @@ class Login extends Component {
         <AppNavbar />
         Login Using:
         <br />
-        <Button><a href="/auth/google">Sign In With Google+</a></Button>
-        <Button><a href="/auth/facebook">Sign in With Facebook</a></Button>
+        <a href="/auth/google">Sign In With Google+</a>
+        <a href="/auth/facebook">Sign in With Facebook</a>
       </div>
     )
   }
