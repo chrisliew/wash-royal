c79da8fb6357da6ce848a2f7ee77fe627256a01f
chrisliew <chriswvliew@gmail.com>
2019-01-17 19:00:17 +0700

    added sendgrid key

--- config/prod.js
+++ config/prod.js
@@ -4,10 +4,11 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   google: {
     clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
+    clientSecret: process.env.CLIENT_SECRET
   },
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
-  email: process.env.EMAIL
-}
+  email: process.env.EMAIL,
+  sendGridKey: process.env.SEND_GRID_KEY
+};
